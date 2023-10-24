17e9afe55b4ae8c6b202b1f9a6349aefbe830469
Ivan Bochenkov <arcticmonkey777@yandex.ru>
2023-10-24 16:57:21 +0200

    Add controller order

---
+++ CRM/Responses/GetOrderResponse.cs
@@ -0,0 +1,21 @@
+﻿using MyCRM.Model;
+
+namespace MyCRM.Responses
+{
+    public class GetOrderResponse 
+    {
+        
+        
+        public int WaiterId { get; set; }
+        public int TableId { get; set; }
+        public DateTime OrderTime { get; set; }
+
+        public GetOrderResponse(Order order)
+        {
+            
+            WaiterId = order.WaiterId;
+            TableId = order.TableId;
+            OrderTime = order.OrderTime;
+        }
+    }
+}
